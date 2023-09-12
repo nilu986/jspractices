5abd10a45875f61f683c73de9a11ccdcfad221f8
nilu986 <nilu.986@gmail.com>
2023-09-12 15:27:09 +0530

    1 file modified

--- practices/progress_bar.js
+++ practices/progress_bar.js
@@ -1,4 +1,21 @@
-const progress_bar_el = document.getElementsByClassName('progress-bar')[0];
+const progressBarEl = document.getElementsByClassName('progress-bar')[0];
+let interval;
+let width = 1;
+
+const widthFunc = () => {
+  if (width >= 100) {
+    clearInterval(interval);
+  } else {
+    width++;
+    progressBarEl.style.width = width + '%';
+  }
+};
+
+(() => {
+  interval = setInterval(widthFunc, 100);
+})();
+
+/* const progress_bar_el = document.getElementsByClassName('progress-bar')[0];
 var i = 0;
 (() => {
   if (i == 0) {
@@ -15,4 +32,4 @@ var i = 0;
       }
     }
   }
-})();
+})(); */
